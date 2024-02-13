eb8d8e222febda65a4823d13dfefa017ecb8ad4c
Joshua <joshua.thomas@verified.com.au>
2024-02-14 10:25:01 +1100

    fix: array to string conversion

--- src/Listeners/LogNotification/MailLogNotificationProcessor.php
+++ src/Listeners/LogNotification/MailLogNotificationProcessor.php
@@ -3,7 +3,6 @@
 namespace Verifiedit\LogNotifications\Listeners\LogNotification;
 
 use Carbon\CarbonImmutable;
-use Illuminate\Config\Repository;
 use Illuminate\Notifications\Events\NotificationSent;
 use Illuminate\Support\Facades\Config;
 use Verifiedit\LogNotifications\Contracts\LogNotificationProcessorContract;
@@ -12,13 +11,14 @@ class MailLogNotificationProcessor implements LogNotificationProcessorContract
 {
     public function process(NotificationSent $event): array
     {
+        $notifiable = $event->notifiable->routeNotificationFor('mail');
         $data = [
             'application' => Config::get('log-notifications.application-name'),
             'channel' => 'mail',
             'reference' => $event->notification->id,
             'serviceCommunications' => json_encode($event->response) ?: '',
             'message' => '',
-            'recipient' => (string)$event->notifiable->routeNotificationFor('mail'),
+            'recipient' => is_array($notifiable) ? (string)json_encode($notifiable) : (string)$notifiable,
             'sentAt' => CarbonImmutable::now(),
         ];
 
